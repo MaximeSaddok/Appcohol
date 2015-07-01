f2d4c373708040f4403d625c4076e910a0170872
dil <uzunov_d@etna-alternance.net>
2015-07-01 16:53:21 +0200

    home

--- www/js/app.js
+++ www/js/app.js
@@ -17,3 +17,28 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+var ionicApp = angular.module('starter', ['ionic']);
+ 
+ionicApp.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
+ 
+ionicApp.controller("ExampleController", function($scope) {
+ 
+    $scope.images = [];
+ 
+    $scope.loadImages = function() {
+        for(var i = 0; i < 4; i++) {
+            $scope.images.push({id: i, src: "http://placehold.it/50x50"});
+        }
+    }
+ 
+});
